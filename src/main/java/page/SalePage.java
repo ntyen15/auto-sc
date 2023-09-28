bcfddcd07aabd3e500f028ba191c0dc581f7d1f2
ntyen <nguyenyen1508@gmail.com>
2023-09-28 17:06:28 +0700

    them cooment

--- src/main/java/page/SalePage.java
+++ src/main/java/page/SalePage.java
@@ -1,8 +1,41 @@
 package page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SalePage {
     private WebDriver driver;
+    private  By itemSearchInput = By.xpath("//input[@class='q-field__native q-placeholder']");
+    private  By selectedButton = By.xpath("//button[@class='q-btn q-btn-item non-selectable no-outline btn btn-action btn-primary q-btn--flat q-btn--rectangle text- q-btn--actionable q-focusable q-hoverable q-btn--wrap']");
+    private By PrintOption = By.xpath("//div[@aria-label='In hóa đơn']");
+    private By paymentButton = By.xpath("//button[@class='q-btn q-btn-item non-selectable no-outline btn-take-money q-btn--standard q-btn--rectangle q-btn--actionable q-focusable q-hoverable q-btn--wrap active']");
     public SalePage(WebDriver driver){this.driver=driver;}
+    public void enterItemSearchKey(String ItemSearchKey){
+        WebElement searchItemBox = driver.findElement(itemSearchInput);
+        if (searchItemBox.isDisplayed())
+            searchItemBox.sendKeys(ItemSearchKey);
+    }
+    public void clickAddItemButton(){
+        WebElement addItem = driver.findElement(selectedButton);
+        if (addItem.isDisplayed())
+            addItem.click();
+    }
+    public void offPrintBill(){
+        WebElement printBill = driver.findElement(PrintOption);
+        if (printBill.isDisplayed())
+            printBill.click();
+    }
+    public void clickPaymentButton(){
+        WebElement payment = driver.findElement(paymentButton);
+        if (payment.isDisplayed())
+            payment.click();
+    }
+    public void payOrder(String ItemSearchKey) throws Exception {
+        enterItemSearchKey(ItemSearchKey);
+        clickAddItemButton();
+        offPrintBill();
+        clickPaymentButton();
+        Thread.sleep(9000);
+    }
 }
--- src/test/java/TestCase/LoginAndCreatInvoice.java
+++ src/test/java/TestCase/LoginAndCreatInvoice.java
@@ -4,16 +4,18 @@ import initWebdriver.InitChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.LoginPage;
+import page.SalePage;
 
-public class Login extends InitChromeDriver {
+public class LoginAndCreatInvoice extends InitChromeDriver {
     @Test
 
     @Override
     public void signInTest() {
         LoginPage loginPage = new LoginPage(super.getDriver());
+        SalePage salePage = new SalePage(super.getDriver());
         System.out.println("Trạng thái mở trang login: " +loginPage.verifySigninPageTitle()); // in trạng thái check mở trang login
         Assert.assertTrue(loginPage.verifySigninPageTitle(), "Khong mở được trang login"); // Kiểm tra mở trang login thành công hay thất bại
-        System.out.println("Mở trang Login thành công");
+        System.out.println("Mở trang LoginAndCreatInvoice thành công");
         // Thực hiện login
 
         try {
@@ -27,9 +29,25 @@ public class Login extends InitChromeDriver {
         System.out.printf(loginPage.getTitel());
         System.out.println("Trạng thái mở trang chủ: " +loginPage.verifyHomePageTitel()); // in trạng thái  mở trang chủ
         System.out.println("Trạng thái login: " +loginPage.verifyHomePageTitel()); // in trạng thái check login
-        Assert.assertTrue(loginPage.verifyHomePageTitel(), "Login thất bại"); // Kiểm tra login thành công hay thất bại
-        System.out.println("Login thành công");
+        Assert.assertTrue(loginPage.verifyHomePageTitel(), "LoginAndCreatInvoice thất bại"); // Kiểm tra login thành công hay thất bại
+        System.out.println("LoginAndCreatInvoice thành công");
+        try {
+            salePage.payOrder("DTO01");
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+        }
+        System.out.printf("Tạo hóa đơn thành công.");
 
 
     }
+//    public void createInvoice(){
+//        SalePage salePage = new SalePage(super.getDriver());
+//        try {
+//            salePage.payOrder("DTO01");
+//        } catch (Exception e) {
+//
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
